5d57368fee7a73e73c9ec82f05f0994700b89109
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2014-02-15 23:10:03 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shelvme::Application.config.secret_key_base = '5424e43b45e82ea99bf90684b4812e060283ce643a4bc00af0646ff0dbea59576e3e6834001c88ab4bce67b48a444dd0cb87b3c38b5ceb0081ff755e918cb93c'
