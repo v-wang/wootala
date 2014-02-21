3c605f067ed3445898f2414e4dea923bc106d15c
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2014-02-21 23:20:39 +0000

    Further changes to production

--- app/models/biofeed.rb
+++ app/models/biofeed.rb
@@ -2,12 +2,5 @@ class Biofeed < ActiveRecord::Base
 belongs_to :user
   
 has_attached_file :image,
-:styles => { :medium => "x300", :thumb => "x100" },
-:storage => :s3,
-:s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+:styles => { :medium => "x300", :thumb => "x100" }
 end
