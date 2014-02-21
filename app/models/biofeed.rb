cec9363c4213550cf0cddcae1e1829be9a224c64
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2014-02-21 23:16:02 +0000

    Even more changes in production

--- app/models/biofeed.rb
+++ app/models/biofeed.rb
@@ -3,9 +3,11 @@ belongs_to :user
   
 has_attached_file :image,
 :styles => { :medium => "x300", :thumb => "x100" },
-:default_url => "postgres://lagrdhsvtavyse:bM8zEjcfLBj0mxBJc_2c5Kxnay@ec2-54-204-47-58.compute-1.amazonaws.com:5432/d4p1djcs546f2o",
 :storage => :s3,
-:bucket => ENV['S3_BUCKET_NAME'],
-:s3_credentials => S3_CREDENTIALS,
-:s3_host_name => 's3-eu-west-1.amazonaws.com'
+:s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
