4d5106432b0c4316eef997ba55eac5afd617c663
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2014-02-21 23:00:04 +0000

    More changes to Production

--- app/models/biofeed.rb
+++ app/models/biofeed.rb
@@ -6,5 +6,6 @@ has_attached_file :image,
 :default_url => "postgres://lagrdhsvtavyse:bM8zEjcfLBj0mxBJc_2c5Kxnay@ec2-54-204-47-58.compute-1.amazonaws.com:5432/d4p1djcs546f2o",
 :storage => :s3,
 :bucket => ENV['S3_BUCKET_NAME'],
-:s3_credentials => S3_CREDENTIALS
+:s3_credentials => S3_CREDENTIALS,
+:s3_host_name => 's3-eu-west-1.amazonaws.com'
 end
